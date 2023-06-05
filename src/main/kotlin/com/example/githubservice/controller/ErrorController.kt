acb7c54b9e2dee409b815601f30446f11af45f05
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-06-05 22:12:37 +0200

    Modify ErrorController

--- src/main/kotlin/com/example/githubservice/controller/ErrorController.kt
+++ src/main/kotlin/com/example/githubservice/controller/ErrorController.kt
@@ -1,24 +1,29 @@
 package com.example.githubservice.controller
 
-import com.example.githubservice.service.UserNotFoundException
+import com.example.githubservice.dto.ExceptionDTO
+import com.example.githubservice.exception.NotAcceptableHeaderException
+import com.example.githubservice.exception.UserNotFoundException
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
-class GlobalExceptionHandler {
+class ErrorController(val objectMapper: ObjectMapper) {
 
     @ExceptionHandler(UserNotFoundException::class)
     fun handleUserNotFoundException(ex: UserNotFoundException): ResponseEntity<String> {
         val errorDTO = ExceptionDTO(404, ex.message)
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO.toString())
+        val jsonErr = objectMapper.writeValueAsString(errorDTO)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(jsonErr)
     }
 
     @ExceptionHandler(NotAcceptableHeaderException::class)
     fun handleNotAcceptableHeaderException(ex: NotAcceptableHeaderException): ResponseEntity<String> {
         val errorDTO = ExceptionDTO(406, ex.message)
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO.toString())
+        val jsonErr = objectMapper.writeValueAsString(errorDTO)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(jsonErr)
     }
 
     @ExceptionHandler(Exception::class)
