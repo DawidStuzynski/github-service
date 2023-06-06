2a60da7c33e260e1076bcb04ce134e307d9ee0b2
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-06-06 16:37:55 +0200

    Refactor RepositoryController

--- src/main/kotlin/com/example/githubservice/controller/RepositoryController.kt
+++ src/main/kotlin/com/example/githubservice/controller/RepositoryController.kt
@@ -1,22 +1,21 @@
 package com.example.githubservice.controller
 
-import com.example.githubservice.model.GitHubRepository
+import com.example.githubservice.dto.RepositoryWithBranchesDto
+import com.example.githubservice.exception.NotAcceptableHeaderException
 import com.example.githubservice.service.RepositoryService
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class RepositoryController(private val service: RepositoryService) {
 
-    @GetMapping("/{username}")
-    fun getRepositories(@PathVariable username: String, @RequestHeader("Accept") acceptHeader: String?): ResponseEntity<List<GitHubRepository>> {
+    @GetMapping("/repository-with-branches/{username}")
+    fun getRepositories(@PathVariable username: String, @RequestHeader("Accept") acceptHeader: String?): List<RepositoryWithBranchesDto> {
 
         if (acceptHeader != null && acceptHeader == "application/xml") {
             throw NotAcceptableHeaderException("application/xml is not supported.")
         } else {
-            val repositories = service.getRepositories(username)
-            return ResponseEntity.accepted().body(repositories)
+            return service.getRepositoryBranchesByUsername(username)
         }
     }
 }
