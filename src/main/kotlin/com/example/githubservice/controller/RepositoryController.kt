4266d0532cbd0505716cb5b59dfae61ee1089be9
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-06-08 09:27:43 +0200

    Set configuration to only accept json

--- src/main/kotlin/com/example/githubservice/controller/RepositoryController.kt
+++ src/main/kotlin/com/example/githubservice/controller/RepositoryController.kt
@@ -1,23 +1,18 @@
 package com.example.githubservice.controller
 
 import com.example.githubservice.dto.RepositoryWithBranchesDTO
-import com.example.githubservice.exception.NotAcceptableHeaderException
 import com.example.githubservice.mappers.DomainToDtoMapper
 import com.example.githubservice.services.RepositoryService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class RepositoryController(private val service: RepositoryService, val mapper: DomainToDtoMapper) {
 
-    @GetMapping("/repository-with-branches/{username}")
+    @GetMapping(value = ["repository-with-branches/{username}"], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun getRepositories(@PathVariable username: String, @RequestHeader("Accept") acceptHeader: String?): List<RepositoryWithBranchesDTO> {
-
-        if (acceptHeader != null && acceptHeader == "application/xml") {
-            throw NotAcceptableHeaderException("application/xml is not supported.")
-        } else {
-            return service.getRepositoryBranchesByUsername(username)
-                    .map { mapper.toDto(it) }
-        }
+        return service.getRepositoryBranchesByUsername(username)
+                .map { mapper.toDto(it) }
     }
 }
